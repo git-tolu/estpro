e8fbab1b31c22218ed16e6c2ccba37178d01a395
git-tolu <adejumobitoluwanimi2@gmail.com>
2023-03-15 13:02:42 +0100

    grabbing device details

--- estpro/lib/login_page.dart
+++ estpro/lib/login_page.dart
@@ -11,7 +11,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 
 class LoginPage extends StatefulWidget {
-  LoginPage({super.key});
+  const LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
